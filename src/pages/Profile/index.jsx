ed68b08306a2f3a8cda4f9653f0e130825fb217e
XiphlR <monthon.workspace@gmail.com>
2026-04-05 22:06:31 +0700

    Update index.jsx

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -2,16 +2,14 @@ function Profile() {
   return (
     <div className="card profile-card">
       <img
-        src="https://via.placeholder.com/150"
+        src=""
         alt="Profile Avatar"
         className="avatar"
       />
-      <h2>ชื่อของคุณ</h2>
-      <h4 className="role">Software Developer & Gamer</h4>
+      <h2>มณฑล แก้วบริวงษ์</h2>
+      <h4 className="role">Multimedia and Game Developer</h4>
       <p className="bio">
-        สวัสดีครับ! ผมเป็นนักพัฒนาที่หลงใหลในการสร้างสรรค์สิ่งใหม่ๆ 
-        ถนัดการสร้างแอปพลิเคชันด้วย <strong>Flutter</strong> และพัฒนาระบบหลังบ้านด้วย <strong>Django</strong> 
-        นอกจากเวลาเขียนโค้ดแล้ว ผมชอบเล่นเกมและสนใจงานศิลปะแบบ Paper Quilling เป็นงานอดิเรก
+
       </p>
     </div>
   );
